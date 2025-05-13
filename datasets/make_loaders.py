0e1ef025c7e829bae30f5fdb7686ed253c9d55bd
Sam Laing <slaing155@gmail.com>
2025-05-13 13:01:44 +0200

    adding random seed dynamics to loader

--- datasets/make_loaders.py
+++ datasets/make_loaders.py
@@ -16,16 +16,24 @@ def make_loaders(
         dataset: Dataset object
         batch_size: Batch size for data loading
         shuffle: Whether to shuffle the data
+        seed: controlling both train-val and data loading generator 
+        val_size: Size of validation set (if None, use the entire dataset for training)
     
     Returns:
         train and val loaders (randomly select a val set with seed ) if val_size is not None else return train loader alone
     """
     assert isinstance(dataset, Dataset), "dataset must be a torch.utils.data.Dataset"
     assert isinstance(batch_size, int), "batch_size must be an integer"
+
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+
+    g = torch.Generator()
+    g.manual_seed(seed)
+
     if val_size is not None:
         # Randomly select a validation set
         indices = np.arange(len(dataset))
-        np.random.seed(seed)
         np.random.shuffle(indices)
         val_size = int(len(dataset) * val_size)
         val_indices = indices[:val_size]
@@ -34,11 +42,19 @@ def make_loaders(
         train_dataset = torch.utils.data.Subset(dataset, train_indices)
         val_dataset = torch.utils.data.Subset(dataset, val_indices)
         
-        train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=shuffle)
-        val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=shuffle)
+        train_loader = DataLoader(
+            train_dataset, batch_size=batch_size, shuffle=shuffle, 
+            generator=g if shuffle else None
+            )
+        val_loader = DataLoader(
+            val_dataset, batch_size=batch_size, shuffle=shuffle, 
+            generator=g if shuffle else None
+            )
         
         return train_loader, val_loader
     else:   
         # Use the entire dataset for training
-        train_loader = DataLoader(dataset, batch_size=batch_size, shuffle=shuffle)
+        train_loader = DataLoader(
+            dataset, batch_size=batch_size, shuffle=shuffle, 
+            generator=g if shuffle else None)
         return train_loader
